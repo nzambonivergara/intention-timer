d4fb0235e695613f4067880e52a81e81f7a86646
Nolan Caine <nolancaine@gmail.com>
2021-07-29 18:15:48 -0600

    Update data model on form submit, clean up code

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-// Create classes with image/color for each button
   // Meditate: #C278FD
   // Exercise: #FD8078
   // Error: #EFB7EC
@@ -8,12 +7,14 @@ var studyButton = document.querySelector(".study-button");
 var meditateButton = document.querySelector(".meditate-button");
 var exerciseButton = document.querySelector(".exercise-button");
 var activityButtons = document.querySelectorAll("div.activity-category-div > input");
+
+var taskDescriptionInput = document.getElementById("task-description");
 var minutesInput = document.getElementById("minutes");
 var secondsInput = document.getElementById("seconds");
-var formSubmit = document.querySelector("form");
-var errorMessage = document.querySelector(".error-message");
-var taskDescriptionInput = document.getElementById("task-description");
 var inputFields = [ taskDescriptionInput, minutesInput, secondsInput ];
+var formSubmit = document.querySelector("form");
+
+var errorMessage = document.querySelector(".error-message");
 var warningText = document.querySelectorAll('.warning');
 
 // Event Handlers
@@ -76,183 +77,33 @@ function preventInvalidCharacters() {
 
 function submitForm() {
   event.preventDefault();
-  for (var i=0; i<inputFields.length; i++) {
-    if (!inputFields[i].value) {
+  for (var i=0; i < inputFields.length; i++) {
+    if (!getCategory()) {
+      return;
+    } else if (!inputFields[i].value) {
       warningText[i].innerHTML = `<img src="./assets/warning.svg" alt="warning sign" id="warning"> A ${inputFields[i].name} is required.`;
+      return;
     } else {
-      warningText[i].innerHTML = ``;
-        // create a new activity intance with the form values
+      var newActivity = new Activity(getCategory(), taskDescriptionInput.value, minutesInput.value, secondsInput.value);
+      console.log(newActivity);
+      return;
     }
   }
-
 }
 
-// if we have a variable set to an interpolated string of that
-// description field errro;
-// for loop checking to see if any fields are empty
-// conditional statement (!inputFields[i].value) {
-  // append <p><img src="./assets/warning.svg" alt="warning sign" id="warning"> A ${inputFields[i].id} is required.</p>
-
-// Write an html element <p> <img> </p>
- // Add a class of hidden
-// get the form
-// add event listener when "submit"
-// event handler -- showErrorMessage
-// if empty fields,
-// Remove hidden class// Create classes with image/color for each button
-  // Meditate: #C278FD
-  // Exercise: #FD8078
-  // Error: #EFB7EC
-
-// Query Selectors
-var studyButton = document.querySelector(".study-button");
-var meditateButton = document.querySelector(".meditate-button");
-var exerciseButton = document.querySelector(".exercise-button");
-var activityButtons = document.querySelectorAll("div.activity-category-div > input");
-var minutesInput = document.getElementById("minutes");
-var secondsInput = document.getElementById("seconds");
-var formSubmit = document.querySelector("form");
-var errorMessage = document.querySelector(".error-message");
-var taskDescriptionInput = document.getElementById("task-description");
-var inputFields = [ taskDescriptionInput, minutesInput, secondsInput ];
-var warningText = document.querySelectorAll('.warning');
-
-// Event Handlers
-// studyButton.addEventListener("click", activateStudy);
-// meditateButton.addEventListener("click", activateMeditate);
-// exerciseButton.addEventListener("click", activateExercise);
-for (var i = 0; i < activityButtons.length; i++) {
-  activityButtons[i].addEventListener("click", activateButton);
-};
-
-for (var i = 0; i < inputFields.length; i++) {
-  inputFields[i].addEventListener("keyup", detectKeyInput);
-};
-
-minutesInput.addEventListener("keydown", preventInvalidCharacters);
-secondsInput.addEventListener("keydown", preventInvalidCharacters);
-formSubmit.addEventListener("submit", submitForm);
-
-// Global vriables
-var invalidCharacters = [ "-", "+", "e", " " ];
-
-
-// Functions
-function removeActivation() {
-  studyButton.classList.remove("study-button-clicked");
-  meditateButton.classList.remove("meditate-button-clicked");
-  exerciseButton.classList.remove("exercise-button-clicked");
+function getCategory() {
   for (var i = 0; i < activityButtons.length; i++) {
-    activityButtons[i].classList.remove('checked');
-  }
-};
-
-function activateButton(event) {
-  removeActivation();
-  if (event.target.value === "Study") {
-    studyButton.classList.add("study-button-clicked");
-    studyButton.checked = true;
-  } else if (event.target.value === "Meditate") {
-    meditateButton.classList.add("meditate-button-clicked");
-    meditateButton.checked = true;
-  } else if (event.target.value === "Exercise") {
-    exerciseButton.classList.add("exercise-button-clicked");
-    exerciseButton.checked = true;
-  }
-};
-
-function detectKeyInput() {
-  for (var i = 0; i < inputFields.length; i++) {
-    if (inputFields[i].value) {
-      warningText[i].innerHTML = ``;
+    if (activityButtons[i].checked) {
+      return activityButtons[i].value;
     }
   }
-};
-
-function preventInvalidCharacters() {
-  if (invalidCharacters.includes(event.key)) {
-    event.preventDefault();
-  }
-};
-
-function submitForm() {
-  event.preventDefault();
-  for (var i=0; i<inputFields.length; i++) {
-    if (!inputFields[i].value) {
-      warningText[i].innerHTML = `<img src="./assets/warning.svg" alt="warning sign" id="warning"> A ${inputFields[i].name} is required.`;
-    } else {
-      warningText[i].innerHTML = ``;
-        // create a new activity intance with the form values
-    }
-  }
-
+  return false;
 }
 
-// if we have a variable set to an interpolated string of that
-// description field errro;
-// for loop checking to see if any fields are empty
-// conditional statement (!inputFields[i].value) {
-  // append <p><img src="./assets/warning.svg" alt="warning sign" id="warning"> A ${inputFields[i].id} is required.</p>
-
-// Write an html element <p> <img> </p>
- // Add a class of hidden
-// get the form
-// add event listener when "submit"
-// event handler -- showErrorMessage
-// if empty fields,
-// Remove hidden class
-// if not empty,
-// add hidden class
 
 
-// function activateStudy() {
-//   studyButton.classList.add("study-button-clicked");
-// };
-//
-// function activateMeditate() {
-//   meditateButton.classList.add("meditate-button-clicked");
-// }
-//
-// function activateExercise() {
-//   exerciseButton.classList.add("exercise-button-clicked");
-// }
 
 
-// Change the input type to number
-// Target the minutes and seconds input fields
-// Declare an array of invalid characters
-// Add event listener to input fields that prevents those characters from being added
-
-//
-//
-// function logTarget(event) {
-//   console.log(event.target);
-// }
-
-// if not empty,
-// add hidden class
-
-
-// function activateStudy() {
-//   studyButton.classList.add("study-button-clicked");
-// };
-//
-// function activateMeditate() {
-//   meditateButton.classList.add("meditate-button-clicked");
-// }
-//
-// function activateExercise() {
-//   exerciseButton.classList.add("exercise-button-clicked");
-// }
-
-
-// Change the input type to number
-// Target the minutes and seconds input fields
-// Declare an array of invalid characters
-// Add event listener to input fields that prevents those characters from being added
-
-//
-//
-// function logTarget(event) {
-//   console.log(event.target);
-// }
+function logTarget(event) {
+  console.log(event.target);
+}
