e73b2f836a8039b800ba0bff567e868dcd653d0a
Joshua Horner <lesliewbusiness@gmail.com>
2021-07-31 18:25:19 -0600

    Add floor and complete state to timer

--- Activity.js
+++ Activity.js
@@ -9,14 +9,6 @@ class Activity {
   }
 
   startTimer() {
-    //  Function uses minutes and seconds
-      //  Minutes and seconds will be pulled from class instance
-    //  Every time the function runs, time variable is decreased
-      //  Declare time in constructor
-        //  Time is number of minutes * 60 + seconds
-    //  Reassign this.minutes and this.seconds to updated time
-      //  this.minutes is reassigned to time variable / 60 to get minutes
-    console.log(this.time);
     if (this.time > -1) {
       this.minutes = Math.floor(this.time / 60);
       this.seconds = this.time % 60;
@@ -27,14 +19,11 @@ class Activity {
         }
       this.time--;
     } else {
+      console.log(this.time);
+      clearInterval(countdown);
       startTimer.innerText = "COMPLETE!";
       return;
     }
-        //  uses Math.floor() to ensure seconds are not accounted for
-    //  Reassign this.seconds to % 60
-
-    //  var timer will need to display updated times through innerHTML
-      //  function will update time through setInterval()
   }
 
   markComplete() {
